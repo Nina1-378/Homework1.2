ae8746a69cfb86ca8f54e90f1983c83292bab17d
AmstaF <ninani-na@mail.ru>
2022-08-12 18:03:11 +0300

    Исправила замечания

--- src/Main.java
+++ src/Main.java
@@ -25,38 +25,38 @@ public class Main {
         System.out.println("Задание 2");
         float boxer1 = 78.2f;
         float boxer2 = 82.7f;
-            float boxersWeight = boxer1 + boxer2;
+        float boxersWeight = boxer1 + boxer2;
         System.out.println("Общий вес двух бойцов " + boxersWeight + " кг");
-            float differenceBetweenWeight = boxer2 - boxer1;
+        float differenceBetweenWeight = Math.abs (boxer1 - boxer2);
         System.out.println("Разница между весами бойцов " + differenceBetweenWeight + " кг");
 
         System.out.println("Задание 3");
         byte bananas = 5;
         byte bananasWeight = 80;
-            int totalBananasWeight = bananas * bananasWeight;
+        int totalBananasWeight = bananas * bananasWeight;
         System.out.println("Вес бананов = " + totalBananasWeight + " г");
         short milk = 200;
         short milkWeight = 105;
-            int totalMilkWeight = (milk / 100) * milkWeight;
+        int totalMilkWeight = (milk / 100) * milkWeight;
         System.out.println("Вес молока = " + totalMilkWeight + " г");
         byte iceCream = 2;
         short iceCreamWeight = 100;
-            int totalIceCreamsWeight = iceCream * iceCreamWeight;
+        int totalIceCreamsWeight = iceCream * iceCreamWeight;
         System.out.println("Вес мороженного = " + totalIceCreamsWeight + " г");
         byte eggs = 4;
         short eggsWeight = 70;
-            int totalEggsWeight = eggs * eggsWeight;
+        int totalEggsWeight = eggs * eggsWeight;
         System.out.println("Вес яиц = " + totalEggsWeight + " г");
         int sportsBreakfastTotalWeight = totalBananasWeight + totalMilkWeight + totalIceCreamsWeight + totalEggsWeight;
         System.out.println("Общий вес спорт-завтрака в граммах = " + sportsBreakfastTotalWeight + " г");
-        int grPerKg = 1000;
-        float sportsBreakfastTotalWeight2 = sportsBreakfastTotalWeight / (float) grPerKg;
+        int grInKg = 1_000;
+        float sportsBreakfastTotalWeight2 = sportsBreakfastTotalWeight / (float) grInKg;
         System.out.println("Общий вес спорт-завтрака в килограммах = " + sportsBreakfastTotalWeight2 + " кг");
 
         System.out.println("Задание 4");
         byte needReset = 7;
-        int kgPerGr = 1000;
-        float weightGr = needReset * (float) kgPerGr;
+        int kgInGr = 1_000;
+        float weightGr = needReset * (float) kgInGr;
         int weightLoss1 = 250;
         int weightLoss2 = 500;
         float daysWeightLoss1 = weightGr / weightLoss1;
@@ -68,58 +68,66 @@ public class Main {
 
         System.out.println("Задание 5");
         float salary = 12_127.50f;
-        float salaryМаша = 67_760.00f;
-        float salaryДенис = 83_690.00f;
-        float salaryКристина = 76_230.00f;
+        float salaryMasha = 67_760.00f;
+        float salaryDenis = 83_690.00f;
+        float salaryKristina = 76_230.00f;
         float additionalPayment = 0.1f;
         byte experience = 3;
         float experience1 = 4f;
         float a = salary;
         float y = additionalPayment;
-            if (experience1 <= experience) {
-                float salary1 = a;
+        if (experience1 <= experience) {
+            float salary1 = a;
             System.out.println("Зарплата = " + salary1 + " руб.");
-        }
-            else if ( experience1 > experience ) {
-                float salary1 = a + (a * y);
+        } else if (experience1 > experience) {
+            float salary1 = a + (a * y);
             System.out.println("Теперь зарплата = " + salary1 + " руб.");
         }
         if (experience1 <= experience) {
-            float salaryМаша1 = salaryМаша;
-            float salaryДенис1 = salaryДенис;
-            float salaryКристина1 = salaryКристина;
-            System.out.println("Зарплата Маши = " + salaryМаша1 + " руб.");
-            System.out.println("Зарплата Дениса = " + salaryДенис1 + " руб.");
-            System.out.println("Зарплата Кристины = " + salaryКристина1 + " руб.");
+            float salaryMasha1 = salaryMasha;
+            float salaryDenis1 = salaryDenis;
+            float salaryKristina1 = salaryKristina;
+            System.out.println("Зарплата Маши = " + salaryMasha1 + " руб.");
+            System.out.println("Зарплата Дениса = " + salaryDenis1 + " руб.");
+            System.out.println("Зарплата Кристины = " + salaryKristina1 + " руб.");
+        } else if (experience1 > experience) {
+            float salaryMasha1 = salaryMasha + (float) (salaryMasha * y);
+            float salaryDenis1 = salaryDenis + (float) (salaryDenis * y);
+            float salaryKristina1 = salaryKristina + (float) (salaryKristina * y);
+            System.out.println("Теперь зарплата Маши = " + salaryMasha1 + " руб.");
+            System.out.println("Теперь зарплата Дениса = " + salaryDenis1 + " руб.");
+            System.out.println("Теперь зарплата Кристины = " + salaryKristina1 + " руб.");
         }
-        else if ( experience1 > experience ) {
-            float salaryМаша1 = salaryМаша + (float) (salaryМаша * y);
-            float salaryДенис1 = salaryДенис + (float) (salaryДенис * y);
-            float salaryКристина1 = salaryКристина + (float) (salaryКристина * y);
-            System.out.println("Теперь зарплата Маши = " + salaryМаша1 + " руб.");
-            System.out.println("Теперь зарплата Дениса = " + salaryДенис1 + " руб.");
-            System.out.println("Теперь зарплата Кристины = " + salaryКристина1 + " руб.");
-        }
-        float ar = salaryМаша;
-        float br = salaryДенис;
-        float cr = salaryКристина;
+        float ar = salaryMasha;
+        float br = salaryDenis;
+        float cr = salaryKristina;
         float totalAnnualIncomeBeforeIncrease = (ar + br + cr) * 12;
-        float totalAnnualIncomeAfterIncrease = totalAnnualIncomeBeforeIncrease + (totalAnnualIncomeBeforeIncrease * y);
+        float totalAnnualIncomeAfterIncrease = totalAnnualIncomeBeforeIncrease +
+                (totalAnnualIncomeBeforeIncrease * y);
         float differenceBetweenTheAnnualIncome = totalAnnualIncomeAfterIncrease - totalAnnualIncomeBeforeIncrease;
-                System.out.println("Общий годовой доход до повышения = " + totalAnnualIncomeBeforeIncrease + " руб.");
-                System.out.println("Общий годовой доход после повышения = " + totalAnnualIncomeAfterIncrease + " руб.");
-                System.out.println("Разница между годовым доходом на " + differenceBetweenTheAnnualIncome + " руб.");
-        float totalAnnualIncomeBeforeIncreaseМаши = ar * 12;
-        float totalAnnualIncomeAfterIncreaseМаши = totalAnnualIncomeBeforeIncreaseМаши + (totalAnnualIncomeBeforeIncreaseМаши * y);
-        float differenceBetweenTheAnnualIncomeМаши = totalAnnualIncomeAfterIncreaseМаши - totalAnnualIncomeBeforeIncreaseМаши;
-        float totalAnnualIncomeBeforeIncreaseДениса = br * 12;
-        float totalAnnualIncomeAfterIncreaseДениса = totalAnnualIncomeBeforeIncreaseДениса + (totalAnnualIncomeBeforeIncreaseДениса * y);
-        float differenceBetweenTheAnnualIncomeДениса = totalAnnualIncomeAfterIncreaseДениса - totalAnnualIncomeBeforeIncreaseДениса;
-        float totalAnnualIncomeBeforeIncreaseКристины = cr * 12;
-        float totalAnnualIncomeAfterIncreaseКристины = totalAnnualIncomeBeforeIncreaseКристины + (totalAnnualIncomeBeforeIncreaseКристины * y);
-        float differenceBetweenTheAnnualIncomeКристины = totalAnnualIncomeAfterIncreaseКристины - totalAnnualIncomeBeforeIncreaseКристины;
-        System.out.println("Теперь годовой доход Маши = " + totalAnnualIncomeAfterIncreaseМаши + " рублей" + "." + "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeМаши + "руб.");
-        System.out.println("Теперь годовой доход Дениса = " + totalAnnualIncomeAfterIncreaseДениса + " рублей" + "." + "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeДениса + "руб.");
-        System.out.println("Теперь годовой доход Кристины = " + totalAnnualIncomeAfterIncreaseКристины + " рублей" + "." + "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeКристины + "руб.");
+        System.out.println("Общий годовой доход до повышения = " + totalAnnualIncomeBeforeIncrease + " руб.");
+        System.out.println("Общий годовой доход после повышения = " + totalAnnualIncomeAfterIncrease + " руб.");
+        System.out.println("Разница между годовым доходом на " + differenceBetweenTheAnnualIncome + " руб.");
+        float totalAnnualIncomeBeforeIncreaseMasha = ar * 12;
+        float totalAnnualIncomeAfterIncreaseMasha = totalAnnualIncomeBeforeIncreaseMasha +
+                (totalAnnualIncomeBeforeIncreaseMasha * y);
+        float differenceBetweenTheAnnualIncomeMasha = totalAnnualIncomeAfterIncreaseMasha -
+                totalAnnualIncomeBeforeIncreaseMasha;
+        float totalAnnualIncomeBeforeIncreaseDenis = br * 12;
+        float totalAnnualIncomeAfterIncreaseDenis = totalAnnualIncomeBeforeIncreaseDenis +
+                (totalAnnualIncomeBeforeIncreaseDenis * y);
+        float differenceBetweenTheAnnualIncomeDenis = totalAnnualIncomeAfterIncreaseDenis -
+                totalAnnualIncomeBeforeIncreaseDenis;
+        float totalAnnualIncomeBeforeIncreaseKristina = cr * 12;
+        float totalAnnualIncomeAfterIncreaseKristina = totalAnnualIncomeBeforeIncreaseKristina +
+                (totalAnnualIncomeBeforeIncreaseKristina * y);
+        float differenceBetweenTheAnnualIncomeKristina = totalAnnualIncomeAfterIncreaseKristina -
+                totalAnnualIncomeBeforeIncreaseKristina;
+        System.out.println("Теперь годовой доход Маши = " + totalAnnualIncomeAfterIncreaseMasha + " рублей" + "." +
+                "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeMasha + "руб.");
+        System.out.println("Теперь годовой доход Дениса = " + totalAnnualIncomeAfterIncreaseDenis + " рублей" + "." +
+                "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeDenis + "руб.");
+        System.out.println("Теперь годовой доход Кристины = " + totalAnnualIncomeAfterIncreaseKristina + " рублей" +
+                "." + "Годовой доход вырос на " + differenceBetweenTheAnnualIncomeKristina + "руб.");
     }
 }
